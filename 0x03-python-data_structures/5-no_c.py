c8959f4fb1db8b33bd8208f83c51a0308333a153
codeAKstan <boystanismoses@gmail.com>
2023-12-03 23:53:25 -0800

    Simplified task 5 using list comprehension

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    remove_c = ""
-    if my_string:
-        for c in my_string:
-            if c.lower() != 'c':
-                remove_c += c
-        return remove_c
+    return ''.join(c for c in my_string if c.lower() != 'c')
