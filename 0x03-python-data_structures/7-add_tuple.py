1ca10c5ff035e46066fda7af9ab0691f212b9083
codeAKstan <boystanismoses@gmail.com>
2023-12-04 16:29:01 -0800

    checked the pycodestyle for task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3                                                       
+#!/usr/bin/python3
+
 def add_tuple(tuple_a=(), tuple_b=()):
     a = tuple_a + (0, 0)[:2 - len(tuple_a)]
     b = tuple_b + (0, 0)[:2 - len(tuple_b)]
