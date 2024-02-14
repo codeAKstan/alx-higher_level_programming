54afae8d52a96912b370f26fa09bb07b87a41cb1
codeAKstan <boystanismoses@gmail.com>
2024-02-13 19:22:59 -0800

    Added all mandatory task

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- change the value of an entry
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = 'Bob';
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- calculates the average of all records
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
