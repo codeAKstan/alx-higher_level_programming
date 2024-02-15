a1cbdbd0e78cfa45162ec11ea5ebc13674e0da25
codeAKstan <boystanismoses@gmail.com>
2024-02-14 18:30:10 -0800

    Added task 6 and 7

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- script that creates db and table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (`id` INT AUTO_INCREMENT  UNIQUE NOT NULL PRIMARY KEY,
+`name` VARCHAR(256) NOT NULL);
