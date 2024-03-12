448c20bedbd5061d6e9057497c91d6bfabf20e32
codeAKstan <boystanismoses@gmail.com>
2024-03-12 03:26:38 -0700

    Modified task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-if (argv[2] === undefined || argv[3] === undefined) {
-  console.log('undefined is undefined');
-} else {
-  console.log(argv[2] + ' is ' + argv[3]);
-}
+const farg = argv[2] || 'undefined';
+const sarg = argv[3] || 'undefined';
+
+console.log(`${farg} is ${sarg}`);
