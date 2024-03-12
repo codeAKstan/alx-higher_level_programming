8c8cb23049a8d5782bdeffc6255405fa2a89ba30
codeAKstan <boystanismoses@gmail.com>
2024-03-11 21:56:16 -0700

    Made some modification

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,7 +4,5 @@ const { argv } = require('node:process');
 if (argv[2] === undefined) {
   console.log('No argument');
 } else {
-  argv.slice(2).forEach((val, index) => {
-    console.log(`${val}`);
-  });
+    console.log(argv[2]);
 }
