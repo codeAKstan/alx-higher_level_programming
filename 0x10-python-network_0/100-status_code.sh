6033bdc072ed013f20fe3ff10a635947269147c9
codeAKstan <boystanismoses@gmail.com>
2024-03-31 01:28:23 -0700

    Modified task 7

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL passed as an argument using curl
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1"); echo "$response"
+curl -s -o /dev/null -w "%{http_code}" "$1"
