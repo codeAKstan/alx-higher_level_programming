b06743c728295b9a27fdf77589bdc6a624474294
codeAKstan <boystanismoses@gmail.com>
2024-03-31 00:45:32 -0700

    Added task 5

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script takes a URL as an argument, sends a POST request to the URL using curl,
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
